fd682892b9d6d34849c2941362cb71d9ce0ec972
fistkim101 <fistkim101@gmail.com>
2023-10-10 13:17:40 +0900

    feat: 프로퍼티 설정 등 처리

--- src/main/java/com/fistkim/saving/CleanArchitectureStudyMonolithicApplication.java
+++ src/main/java/com/fistkim/saving/CleanArchitectureStudyMonolithicApplication.java
@@ -2,8 +2,10 @@ package com.fistkim.saving;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CleanArchitectureStudyMonolithicApplication {
 
     public static void main(String[] args) {
